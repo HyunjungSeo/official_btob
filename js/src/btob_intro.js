d682448635f6b29d14791ee9b34bbe2926f0bc95
hyunjungSeo <lamon5246@gmail.com>
2018-03-16 15:03:22 +0900

    re upload 3p make

---
+++ js/src/btob_intro.js
@@ -0,0 +1,15 @@
+(function($){
+// jQuery start ===============
+
+var headBtn = $('#headBtn');
+var showGnbBox = $('#showGnbBox');
+
+showGnbBox.hide();
+
+headBtn.on('click',function(evt){
+	evt.preventDefault();
+	showGnbBox.slideToggle();
+});
+
+// jQuery end =================
+})(this.jQuery);
